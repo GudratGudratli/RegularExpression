f51d4aae4e2770a1c56994170d7e854db6caa5d8
Kudretizade <104269357+Kudretizade@users.noreply.github.com>
2022-11-04 18:56:33 +0400

    Regular Expression - Polindrome

---
+++ RegularExpression/RegularExpression/Program.cs
@@ -0,0 +1,14 @@
+﻿int n, r, sum = 0, temp;
+Console.Write("Eded daxil edin: ");
+n = int.Parse(Console.ReadLine());
+temp = n;
+while (n > 0)
+{
+    r = n % 10;
+    sum = (sum * 10) + r;
+    n /= 10;
+}
+if (temp == sum)
+    Console.Write("Eded polindromdur.");
+else
+    Console.Write("Eded polindrom deil");
